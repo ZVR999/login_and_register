a6e98074062961d55825b8b615a3bb0fb7c700a4
Zachkery Rodriguez <39169752+ZVR999@users.noreply.github.com>
2018-08-20 20:43:32 -0500

    re/added route
    Imported re for email validation.
    Added route /process to first process the information being sent from /index and then sending to /success once completed.
    All information from the registration form is being sent with no issues and are being printed to terminal.

--- server.py
+++ server.py
@@ -1,12 +1,25 @@
 from flask import Flask, render_template, session, redirect, request
 from mysqlconnection import MySQLConnector
-
+import re
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+# r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)" from emailregex.com
 app = Flask(__name__)
 mysql = MySQLConnector(app, 'logindb')
 # Route to the login/registration page
 @app.route('/')
 def index():
     return render_template('index.html')
+
+#Route to process the login/registration information
+@app.route('/process', methods=['POST'])
+def process():
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    email = request.form['email']
+    password = request.form['password']
+    
+    print first_name, last_name, email, password
+    return redirect('/success')
 
 # Route to the success/confirmation page
 @app.route('/success')
